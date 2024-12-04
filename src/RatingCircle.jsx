fd6d36c77295f5f86405ee2bb0f6eaf0fa19e36f
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-12-04 09:17:35 -0500

    Add keyboard interaction to RatingCircle component ♻️

--- src/RatingCircle.jsx
+++ src/RatingCircle.jsx
@@ -1,26 +1,35 @@
-import PropTypes from "prop-types";
-import "./RatingCircle.css";
+import PropTypes from 'prop-types'
+import './RatingCircle.css'
 
 const RatingCircle = ({ children, onClick, isSelected }) => {
   const handleRatingCircleClick = () => {
-    onClick();
-  };
+    onClick()
+  }
 
   const ratingCircleClassName = isSelected
-    ? "rating-circle is-selected"
-    : "rating-circle";
+    ? 'rating-circle is-selected'
+    : 'rating-circle'
 
   return (
-    <div className={ratingCircleClassName} onClick={handleRatingCircleClick}>
-      <p className="rating-circle__number">{children}</p>
+    <div
+      className={ratingCircleClassName}
+      onClick={handleRatingCircleClick}
+      onKeyDown={event => {
+        if (event.key === 'Enter') {
+          handleRatingCircleClick()
+        }
+      }}
+      tabIndex={0}
+    >
+      <p className='rating-circle__number'>{children}</p>
     </div>
-  );
-};
+  )
+}
 
 RatingCircle.propTypes = {
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func.isRequired,
-  isSelected: PropTypes.bool.isRequired,
-};
+  isSelected: PropTypes.bool.isRequired
+}
 
-export default RatingCircle;
+export default RatingCircle
